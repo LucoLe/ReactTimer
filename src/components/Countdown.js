0605eec9b563850aa36db387b171a1c310ecb8aa
Lyudmil Lesinski <lyudmilles@gmail.com>
2017-02-16 10:05:51 +0200

    Add Timer and Countdown components

---
+++ src/components/Countdown.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Countdown extends Component {
+  render() {
+    return (
+      <p>Countdown</p>
+    );
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import Main from './components/Main';
+import Timer from './components/Timer';
+import Countdown from './components/Countdown';
 
 export default (
   <Route path="/" component={Main}>
+    <IndexRoute component={Timer} />
+    <Route path="countdown" component={Countdown} />
   </Route>
 );
