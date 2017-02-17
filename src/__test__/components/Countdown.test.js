342734f76f17ebade14098780efcaeed17a22ddf
Lyudmil Lesinski <lyudmilles@gmail.com>
2017-02-17 18:37:00 +0200

    Add countdown functionality

---
+++ src/__test__/components/Countdown.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
+import Countdown from '../../components/Countdown';
+
+describe('Countdown', () => {
+  test('should exist', () => {
+    const countdown = shallow(<Countdown />);
+  });
+
+  describe('handleSetCountdown', () => {
+    test('should set state to started and countdown', (done) => {
+      const countdown = shallow(<Countdown />);
+
+      countdown.instance().handleSetCountdown(10);
+      expect(countdown.state()).toEqual({count: 10, countdownStatus: 'started'});
+
+      setTimeout(() => {
+        expect(countdown.state()).toEqual({count: 9, countdownStatus: 'started'});
+        done();
+      }, 1001);
+    });
+
+    test('should never set count to less than 0', (done) => {
+      const countdown = shallow(<Countdown />);
+
+      countdown.instance().handleSetCountdown(1);
+
+      setTimeout(() => {
+        expect(countdown.state()).toEqual({count: 0, countdownStatus: 'started'});
+        done();
+      }, 1001);
+    });
+  });
+});
--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -7,14 +7,37 @@ export default class Countdown extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      count: 0,
+      countdownStatus: 'stopped'
     };
 
     this.handleSetCountdown = this.handleSetCountdown.bind(this);
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
+  }
+
+  startTimer() {
+    this.timer = setInterval(() => {
+      let newCount = this.state.count - 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      });
+    }, 1000);
+  }
+
   handleSetCountdown(seconds) {
-    this.setState({count: seconds});
+    this.setState({
+      count: seconds,
+      countdownStatus: 'started'
+    });
   }
 
   render() {
